1389911a364e9f969562e1fe1cf0de1c96d88ec5
adyenpayments <support@adyen.com>
2015-04-09 17:54:37 +0200

    Added authorise3d examples

---
+++ src/com/adyen/examples/api/Authorise3dSecurePaymentHttpPost.java
@@ -0,0 +1,168 @@
+package com.adyen.examples.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * Authorise 3D Secure payment (SOAP)
+ * 
+ * 3D Secure (Verifed by VISA / MasterCard SecureCode) is an additional authentication
+ * protocol that involves the shopper being redirected to their card issuer where their
+ * identity is authenticated prior to the payment proceeding to an authorisation request.
+ * 
+ * In order to start processing 3D Secure transactions, the following changes are required:
+ * 1. Your Merchant Account needs to be confgured by Adyen to support 3D Secure. If you would
+ *    like to have 3D Secure enabled, please submit a request to the Adyen Support Team (support@adyen.com).
+ * 2. Your integration should support redirecting the shopper to the card issuer and submitting
+ *    a second API call to complete the payment.
+ *
+ * This example demonstrates the second API call to complete the payment using SOAP.
+ * See the API Manual for a full explanation of the steps required to process 3D Secure payments.
+ * 
+ * Please note: using our API requires a web service user. Set up your Webservice user:
+ * Adyen CA >> Settings >> Users >> ws@Company. >> Generate Password >> Submit
+ * 
+ * @link /2.API/HttpPost/Authorise3dSecurePayment
+ * @author Created by Adyen - Payments Made Easy
+ */
+
+@WebServlet(urlPatterns = { "/2.API/HttpPost/Authorise3dSecurePayment" })
+public class Authorise3dSecurePaymentHttpPost extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		/**
+		 * HTTP Post settings
+		 * - apiUrl: URL of the Adyen API you are using (Test/Live)
+		 * - wsUser: your web service user
+		 * - wsPassword: your web service user's password
+		 */
+		String apiUrl = "https://pal-test.adyen.com/pal/adapter/httppost";
+		String wsUser = "YourWSUser";
+		String wsPassword = "YourWSUserPassword";
+
+		/**
+		 * Create HTTP Client (using Apache HttpComponents library) and set up Basic Authentication
+		 */
+		CredentialsProvider provider = new BasicCredentialsProvider();
+		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(wsUser, wsPassword);
+		provider.setCredentials(AuthScope.ANY, credentials);
+
+		HttpClient client = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
+
+		/**
+		 * After the shopper's identity is authenticated by the issuer, they will be returned to your
+		 * site by sending an HTTP POST request to the TermUrl containing the MD parameter and a new
+		 * parameter called PaRes (see API manual). These will be needed to complete the payment.
+		 *
+		 * To complete the payment, a PaymentRequest3d should be submitted to the authorise3d action
+		 * of the web service. The request should contain the following variables:
+		 * 
+		 * <pre>
+		 * - merchantAccount: This should be the same as the Merchant Account used in the original authorise request.
+		 * - browserInfo:     It is safe to use the values from the original authorise request, as they
+		                      are unlikely to change during the course of a payment.
+		 * - md:              The value of the MD parameter received from the issuer.
+		 * - paReponse:       The value of the PaRes parameter received from the issuer.
+		 * - shopperIP:       The IP address of the shopper. We recommend that you provide this data, as
+		                      it is used in a number of risk checks, for example, the number of payment
+		                      attempts and location based checks.
+		* </pre>
+		 */
+		List<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+		Collections.addAll(postParameters,
+			new BasicNameValuePair("action", "Payment.authorise3d"),
+
+			new BasicNameValuePair("paymentRequest3d.merchantAccount", "YourMerchantAccount"),
+			new BasicNameValuePair("paymentRequest3d.browserInfo.userAgent", request.getHeader("User-Agent")),
+			new BasicNameValuePair("paymentRequest3d.browserInfo.acceptHeader", request.getHeader("Accept")),
+			new BasicNameValuePair("paymentRequest3d.md", "31h..........vOXek7w"),
+			new BasicNameValuePair("paymentRequest3d.paResponse", "eNqtmF........wGVA4Ch"),
+			new BasicNameValuePair("paymentRequest3d.shopperIP", "123.123.123.123")
+			);
+
+		/**
+		 * Send the HTTP Post request with the specified variables.
+		 */
+		HttpPost httpPost = new HttpPost(apiUrl);
+		httpPost.setEntity(new UrlEncodedFormEntity(postParameters));
+
+		HttpResponse httpResponse = client.execute(httpPost);
+		String paymentResponse = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+
+		/**
+		 * Keep in mind that you should handle errors correctly.
+		 * If the Adyen platform does not accept or store a submitted request, you will receive a HTTP response with
+		 * status code 500 Internal Server Error. The fault string can be found in the paymentResponse.
+		 */
+		if (httpResponse.getStatusLine().getStatusCode() == 500) {
+			throw new ServletException(paymentResponse);
+		}
+		else if (httpResponse.getStatusLine().getStatusCode() != 200) {
+			throw new ServletException(httpResponse.getStatusLine().toString());
+		}
+
+		/**
+		 * If the payment passes validation a risk analysis will be done and, depending on the outcome, an authorisation
+		 * will be attempted. You receive a payment response with the following fields:
+		 * 
+		 * <pre>
+		 * - pspReference    : Adyen's unique reference that is associated with the payment.
+		 * - resultCode      : The result of the payment. Possible values: Authorised, Refused, Error or Received.
+		 * - authCode        : The authorisation code if the payment was successful. Blank otherwise.
+		 * - refusalReason   : Adyen's mapped refusal reason, populated if the payment was refused.
+		 * </pre>
+		 */
+		Map<String, String> paymentResult = parseQueryString(paymentResponse);
+		PrintWriter out = response.getWriter();
+
+		out.println("Payment Result:");
+		out.println("- pspReference: " + paymentResult.get("paymentResult.pspReference"));
+		out.println("- resultCode: " + paymentResult.get("paymentResult.resultCode"));
+		out.println("- authCode: " + paymentResult.get("paymentResult.authCode"));
+		out.println("- refusalReason: " + paymentResult.get("paymentResult.refusalReason"));
+
+	}
+
+	/**
+	 * Parse the result of the HTTP Post request (will be returned in the form of a query string)
+	 */
+	private Map<String, String> parseQueryString(String queryString) throws UnsupportedEncodingException {
+		Map<String, String> parameters = new HashMap<String, String>();
+		String[] pairs = queryString.split("&");
+
+		for (String pair : pairs) {
+			String[] keyval = pair.split("=");
+			parameters.put(URLDecoder.decode(keyval[0], "UTF-8"), URLDecoder.decode(keyval[1], "UTF-8"));
+		}
+
+		return parameters;
+	}
+
+}
